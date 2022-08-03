bcec5a942615ad6d9fc88b03100d40aaf2d64224
Jade Coonts <perfektjade@gmail.com>
2022-08-03 19:37:53 -0400

    Completed Task 5a.

--- index.js
+++ index.js
@@ -216,10 +216,10 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+  return (km * 0.621371)
 }
-
+console.log('Task 5a:', miles(5));
 
 
 //Task 5b - Feet to CM
