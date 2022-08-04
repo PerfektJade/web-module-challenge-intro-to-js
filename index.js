0cce3d074c51618f14a15088b8f24f4883fb9966
Jade Coonts <perfektjade@gmail.com>
2022-08-03 22:59:11 -0400

    Completed Stretch.

--- index.js
+++ index.js
@@ -302,11 +302,12 @@ HINT - you may need to study tomorrow's content on arrays
 HINT - try looking up the .includes() method
 */
 
-
-function vowelCounter(/*add your code here*/) {
-  /*add your code here*/
+function vowelCounter(str) {
+  const count = str.match(/[aeiou]/gi).length;
+  return count;
 }
 
+console.log('Stretch:', vowelCounter('Alphabet'));
 
 
 /*🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑*/
